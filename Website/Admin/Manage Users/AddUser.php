81b83386fc6b46a22d0dda2904a29832340c8bff
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-04 03:28:59 +0800

    Update AddUser.php

--- Website/Admin/Manage Users/AddUser.php
+++ Website/Admin/Manage Users/AddUser.php
@@ -4,6 +4,7 @@
 <head>
     <title>Manage Users</title>
     <!-- Font Awesome for icons and external CSS for styling -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="AddUser.css">
 </head>
