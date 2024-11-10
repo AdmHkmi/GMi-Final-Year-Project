429687344d17d43bc1d7c1c9d0dfa7171ecde8be
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-10 23:14:15 +0800

    Update EmailUserVerification.php

--- Website/Register/EmailUserVerification.php
+++ Website/Register/EmailUserVerification.php
@@ -29,14 +29,14 @@ function sendVerificationEmail($StudentName, $StudentEmail, $token) {
             <p>Thank you for registering with the <strong>GMi Voting System</strong>. To complete your registration and activate your account, we need to verify your email address.</p>
             <p>Please click the link below to verify your account:</p>
             <p style='text-align: center;'>
-                <a href='http://localhost/Voting System/Website/Register/UserVerificationProcess.php?email=$StudentEmail&token=$token' 
+                <a href='http://www.gmisrce-election.my/Website/Register/UserVerificationProcess.php?email=$StudentEmail&token=$token' 
                    style='display: inline-block; padding: 10px 20px; color: white; background-color: #007bff; text-decoration: none; border-radius: 5px;'>
                    Verify My Account
                 </a>
             </p>
             <p>If you did not create this account, please disregard this email.</p>   
             <p>Best regards,<br>
-            <strong>The GMi Voting System Team</strong></p> 
+            <strong>The GMi SRC E-election Team</strong></p> 
             <p style='font-size: 12px; color: #666;'>This is an automated message, please do not reply to this email.</p>
         ";  
         $mail->send();
