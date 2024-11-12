8628ac40a038c22f30dd9ec40538f2d8f69603eb
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-12 22:51:53 +0800

    Update AdminHomepage.php

--- Website/Admin/Home Page/AdminHomepage.php
+++ Website/Admin/Home Page/AdminHomepage.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION['AdminLoggedIn'])) {
 <head>
     <title>SRC E-Voting GMi Website</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Add this line -->
+    <link rel="icon" type="image/icon" href="../../../Images/favicon.ico">
     <link rel="stylesheet" href="AdminHomepage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
