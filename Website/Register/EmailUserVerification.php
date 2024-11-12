f0b9d1f71d9b7baff71c7a331bc0ef708e628f60
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-12 13:48:44 +0800

    Update EmailUserVerification.php

--- Website/Register/EmailUserVerification.php
+++ Website/Register/EmailUserVerification.php
@@ -10,11 +10,11 @@ function sendVerificationEmail($StudentName, $StudentEmail, $token) {
     try {
         // Server settings
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'gmisrce-election.my';                       // Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'adamhakimi6670i@gmail.com';            // SMTP username
         $mail->Password   = 'iwtuokefcymlmjzz';                     // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
+        $mail->SMTPSecure = 'tls';         // Enable TLS encryption
         $mail->Port       = 587;                                    // TCP port to connect to
     
         // Recipients
