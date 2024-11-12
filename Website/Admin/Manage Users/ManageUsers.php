ab1879ae9f07d4b3e818177a151677cd55e7989a
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-12 22:53:39 +0800

    Update ManageUsers.php

--- Website/Admin/Manage Users/ManageUsers.php
+++ Website/Admin/Manage Users/ManageUsers.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['AdminLoggedIn'])) {
     <title>Manage Users</title>
     <!-- Link to external stylesheet -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/icon" href="../../../Images/favicon.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="ManageUsers.css">
 </head>
