70c77c0229ac245ce722380cbbd0ac51a90e1d67
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-12 22:52:25 +0800

    Update ManageNews.php

--- Website/Admin/Manage News/ManageNews.php
+++ Website/Admin/Manage News/ManageNews.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['AdminLoggedIn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage News</title>
+    <link rel="icon" type="image/icon" href="../../../Images/favicon.ico">
     <link rel="stylesheet" href="ManageNews.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
