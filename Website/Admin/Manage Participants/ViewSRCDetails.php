97bc6ed6e7090e5ceb4ce1b8b4a019b6a19aeb87
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-12 22:54:35 +0800

    Update ViewSRCDetails.php

--- Website/Admin/Manage Participants/ViewSRCDetails.php
+++ Website/Admin/Manage Participants/ViewSRCDetails.php
@@ -51,6 +51,7 @@ $conn->close();
     <title>Manage Participants</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="icon" type="image/icon" href="../../../Images/favicon.ico">
     <link rel="stylesheet" href="ViewSRCDetails.css">
 </head>
 <body>
