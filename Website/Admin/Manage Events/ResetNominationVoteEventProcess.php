c280401731ef4ca8295b08ba51ff7d2b846f9d35
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-11-13 18:46:19 +0800

    Update ResetNominationVoteEventProcess.php

--- Website/Admin/Manage Events/ResetNominationVoteEventProcess.php
+++ Website/Admin/Manage Events/ResetNominationVoteEventProcess.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['ResetEvent'])) {
     // Retrieve the EventID that needs to be reset
     $ResetEvent = $_POST['ResetEvent'];
     // Prepare the SQL statement to reset StartDate and EndDate to NULL for the specified EventID
-    $reset_event_sql = "UPDATE VSEvents SET StartDate = NULL, EndDate = NULL WHERE EventID = ?";
+    $reset_event_sql = "UPDATE VSEvents SET StartDate = NULL, EndDate = NULL, VoteLimit = NULL WHERE EventID = ?";
     $stmt_reset_event = $conn->prepare($reset_event_sql);
     // Bind the EventID parameter to the SQL statement
     $stmt_reset_event->bind_param("i", $ResetEvent);
