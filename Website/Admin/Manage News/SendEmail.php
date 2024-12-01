569f4d8c8fb1d238e20e83e04cbc68c1c1d8610a
MUHAMMAD ADAM HAKIMI <162420022+AdmHkmi@users.noreply.github.com>
2024-12-01 16:21:00 +0800

    Update SendEmail.php

--- Website/Admin/Manage News/SendEmail.php
+++ Website/Admin/Manage News/SendEmail.php
@@ -25,13 +25,13 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com'; // SMTP server
     $mail->SMTPAuth = true;
-    $mail->Username = 'adamhakimi6670i@gmail.com'; // SMTP username
-    $mail->Password = 'iwtuokefcymlmjzz'; // SMTP password
+    $mail->Username = 'gmisrceelection@gmail.com'; // SMTP username
+    $mail->Password = 'ycwncyptsdelfpzp'; // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
     // Sender information
-    $mail->setFrom('adamhakimi6670i@gmail.com', 'GMi News Notification');
+    $mail->setFrom('gmisrceelection@gmail.com', 'GMi News Notification');
 
     // Check if there are any news articles
     if ($newsResult->num_rows > 0) {
